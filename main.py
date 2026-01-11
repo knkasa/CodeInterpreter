b9da62afcfe3fc98760b06d6aeb44d8a8927dc84
Ken Nakatsukasa <knkasa@gmail.com>
2026-01-12 00:03:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,6 @@ from src.interpreter import program_execution_class
 
 def main():
     Logger()
-    #user_prompt = '''
-    #    I am trying to create a machine learning model using logistic classification.
-    #    Use "users" table and "interaction" table from Redshift.
-    #    The target is the purchase column in interaction table.
-    #    Use whatever features you think is good for the input data.
-    #    Convert categorical columns into binary.  Impute null if exists.
-    #    Do other preprocessing as necessary.
-    #    '''
-    #user_prompt = '''can you do something with "users" table with python?  Just run some simple python code.'''
     user_prompt = os.environ.get("USER_PROMPT")
 
     if not user_prompt:
@@ -23,3 +14,4 @@ def main():
     
 if __name__ == "__main__":
     main()
+
